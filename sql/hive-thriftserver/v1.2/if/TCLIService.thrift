07be885b46c37bdf5b4d2754f4c1ae10f32f710b
wankunde <wankunde@163.com>
2020-08-26 19:24:35 +0800

    update hive-thriftserver protocol for hive-1.1.0-cdh5.13.1

--- sql/hive-thriftserver/v1.2/if/TCLIService.thrift
+++ sql/hive-thriftserver/v1.2/if/TCLIService.thrift
@@ -456,6 +456,9 @@ enum TOperationState {
 
   // The operation is in an pending state
   PENDING_STATE,
+
+  // The operation is in an timedout state
+  TIMEDOUT_STATE,
 }
 
 // A string identifier. This is interpreted literally.
@@ -551,7 +554,7 @@ struct TOperationHandle {
 // which operations may be executed.
 struct TOpenSessionReq {
   // The version of the HiveServer2 protocol that the client is using.
-  1: required TProtocolVersion client_protocol = TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V8
+  1: required TProtocolVersion client_protocol = TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V7
 
   // Username and password for authentication.
   // Depending on the authentication scheme being used,
@@ -570,7 +573,7 @@ struct TOpenSessionResp {
   1: required TStatus status
 
   // The protocol version that the server is using.
-  2: required TProtocolVersion serverProtocolVersion = TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V8
+  2: required TProtocolVersion serverProtocolVersion = TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V7
 
   // Session Handle
   3: optional TSessionHandle sessionHandle
@@ -695,6 +698,9 @@ struct TExecuteStatementReq {
   
   // Execute asynchronously when runAsync is true
   4: optional bool runAsync = false
+
+  // The number of seconds after which the query will timeout on the server
+  5: optional i64 queryTimeout = 0
 }
 
 struct TExecuteStatementResp {
